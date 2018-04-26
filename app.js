36f538d14ca5e1c3d955641e0f16e191cdd56189
Jorgelin Ortiz <jortiz@intellisys.com.do>
2018-04-26 17:21:43 -0400

    Add base to YouTube implementation

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ var raddConfig = {
   }
 }
 
+var generalConfig = {
+	'apostrophe-db': {
+      uri: "mongodb://um7uixx9wsr1zql:pGXgGdhaDc7iWkkjv9nE@bieojfedueylq7c-mongodb.services.clever-cloud.com:27017/bieojfedueylq7c"
+    },
+    'apostrophe-attachments': {
+		uploadfs: {
+			backend: 's3',
+			secret: 'F3Hd9ETk5MEHzryhtbFBbzroIczJJMFjEhEDCK9M',
+			key: 'AKIAJX5733B6BK7QGCAQ',
+			bucket: 'pastoral-salesianos'
+		}
+	}
+} 
+
 var apos = require('apostrophe')({
   shortName: 'test-project',
 
@@ -35,13 +49,25 @@ var apos = require('apostrophe')({
     // `views/` folder of the project
     'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
 
-    'apostrophe-db': raddConfig["apostrophe-db"],
-    'apostrophe-attachments': raddConfig["apostrophe-attachments"],
+    'apostrophe-db': generalConfig["apostrophe-db"],
+    'apostrophe-attachments': generalConfig["apostrophe-attachments"],
      'apostrophe-video-widgets': {},
      'apostrophe-module': {},
      'local-video-widgets': {},
      'apostrophe-pages': {
        deleteFromTrash: true
-     }
+     },
+    'upload-youtube': {
+      "web":{
+        "client_id":"319062300395-4ivpluvpcntgm5mh4soh5pgoo4p84btu.apps.googleusercontent.com",
+        "project_id":"nice-catbird-202020",
+        "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+        "token_uri":"https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret":"DU-cqzSnupXr9YuBW2wBghNl",
+        "redirect_uris":["http://localhost:3000/oauth2callback"],
+        "javascript_origins":["http://localhost:5000","http://localhost:3000"]
+      }
+    }
   }
 });
